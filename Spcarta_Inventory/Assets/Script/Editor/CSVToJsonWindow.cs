8d0d3f0349b58d6d4ebef7d16c2233990f8041c7
김지현 <kjhstar2486@naver.com>
2025-03-24 14:47:26 +0900

    [feat] 에디터 윈도우 - 리스트 입력 가능하게

--- Spcarta_Inventory/Assets/Script/Editor/CSVToJsonWindow.cs
+++ Spcarta_Inventory/Assets/Script/Editor/CSVToJsonWindow.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class CSVToJsonWindow : EditorWindow
 {
+    // 클래스 명 리스트 
+    public List<string> className;
     
+    // 리스트 표시 object
+    private SerializedObject serializedObject;
+    private SerializedProperty classNameProperty;
 
     [MenuItem("Window/CustomEditor/CSV To Json Window")]
     public static void ShowWindow() 
@@ -13,15 +18,25 @@ public class CSVToJsonWindow : EditorWindow
         GetWindow<CSVToJsonWindow>("Csv to Json Editor");
     }
 
+
+    private void OnEnable()
+    {
+        serializedObject = new SerializedObject(this);
+        classNameProperty = serializedObject.FindProperty("className");
+    }
+
     private void OnGUI()
     {
         GUILayout.Label("Csv to Json Editor", EditorStyles.boldLabel);
 
+        // InputField를 리스트로 
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(classNameProperty, true);
+        serializedObject.ApplyModifiedProperties();
 
-
-        if (GUILayout.Button("Convert")) 
+        if (GUILayout.Button("Convert"))
         {
-            
+            CsvToJsonConverter.CsvConverByName(className);
         }
     }
 }
